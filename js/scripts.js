8901a5b3ae4423a563558213525364f45dc78678
Furia <d.hrnndz@gmail.com>
2016-03-01 15:38:36 -0600

    Landing Page

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+/*$(function(){
+    $('.navbar-lb').data('size','big');
+});
+
+$(window).scroll(function(){
+    if($(document).scrollTop() > 0)
+    {
+        if($('.navbar-lb').data('size') == 'big')
+        {
+            $('.navbar-lb').data('size','small');
+            $('.navbar-lb').stop().animate({
+                height: '4em'
+            },600);
+            $('.navbar-lb .logo img').stop().animate({
+                widht: '6em'
+            },600);
+        }
+    }
+    else
+    {
+        if($('.navbar-lb').data('size') == 'small')
+        {
+            $('.navbar-lb').data('size','big');
+            $('.navbar-lb').stop().animate({
+                height: '8em'
+            },600);
+        }
+    }
+});
+*/
